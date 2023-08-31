c83e5fcbce8bc69505eca50793cdb03711d8d201
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-08-31 18:05:21 -0500

    sdc

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-<<<<<<< HEAD
- HEAD
-=======
- Juan
-console.log("Prueba1, Juan");
-
->>>>>>> d43201eabc3690d1cb566b95b249fbcdbb8d7dd7
- HEAD
-
-
- Vicky
-<<<<<<< HEAD
-
-
- Juan
-=======
- main
->>>>>>> d43201eabc3690d1cb566b95b249fbcdbb8d7dd7
+juan
